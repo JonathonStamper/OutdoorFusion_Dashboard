58c230dad34db8157113f2c0530b87957a12d829
JonathonStamper <96175364+JonathonStamper@users.noreply.github.com>
2023-05-24 19:13:39 +0200

    Home page with css

--- my-app/src/Components/HomePage.js
+++ my-app/src/Components/HomePage.js
@@ -1,48 +1,8 @@
 import { useState, useEffect } from "react"
-
+import './Home.css'
 
 export default function Home() {
   const [Product_Groups, setProductGroups] = useState([]);
-  //     const [columns, setColumns] = useState([]);
-
-
-
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch('http://localhost:5000/api/OutdoorFusion/all');
-  //       const data = await response.json();
-  //       const tableColumns = Object.entries(data).map(([table, columns]) => ({
-  //         table,
-  //         columns: Object.keys(columns[0])
-  //       }));
-  //       setColumns(tableColumns);
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //     }
-  //   };
-
-  //   useEffect(() => {
-  //     fetchData();
-  //   }, []);
-
-  //   return (<>
-  //     <h1>Hello</h1>
-  //     <div>
-  //       {columns.map(({ table, columns }) => (
-  //         <div key={table}>
-  //           <h2>{table}</h2>
-  //           <ul>
-  //             {columns.map(column => (
-  //               <li key={column}>{column}</li>
-  //             ))}
-  //           </ul>
-  //         </div>
-  //       ))}
-  //     </div>
-  //     </>
-  //   );
-  // }
-
 
   const fetchProductGroups = () => {
     fetch('http://localhost:5000/api/OutdoorFusion/all')
@@ -52,6 +12,7 @@ export default function Home() {
         const tableColumns = Object.entries(data).map(([table, columns]) => ({
           table,
           columns: Object.keys(columns[0])
+          
         }));
         setProductGroups(tableColumns);
         console.log(data);
@@ -72,14 +33,15 @@ export default function Home() {
         {Product_Groups ? (
           Product_Groups.map((productGroup) => (
             <div className="Product_Group_Container" key={productGroup.table}>
-              <div>
-                <h3>{productGroup.table}</h3>
+              <div id="Product_Group_Container">
+                <h3 >{productGroup.table}</h3>
                 <ul id="ColumnList">
               {productGroup.columns ? (productGroup.columns.map((productGroup_column) => (
                 <li key={productGroup_column}>{productGroup_column}</li>
               ))
             ) : null}
                 </ul>
+                <a href="">Analyze me</a>
               </div>
             </div>
           ))
