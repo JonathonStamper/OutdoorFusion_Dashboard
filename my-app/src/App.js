947c00e86e633bea1cadc83291b58f8e50d41346
JonathonStamper <96175364+JonathonStamper@users.noreply.github.com>
2023-05-24 18:23:12 +0200

    Hompage functional

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,20 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './Nav';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Components/HomePage';
 
 export default function App() {
   return (
     <Router>
-      <>
+      
         <NavBar></NavBar>
         <div>
           <Routes>
-            <Route path="/" element="" />
+            <Route path="/home" element={<Home/>} />
             <Route path="/Perfomances" element="" />
   
           </Routes>
         </div>
-      </>
+      
     </Router>
   );
 }
--- my-app/src/Components/HomePage.js
+++ my-app/src/Components/HomePage.js
@@ -1,39 +1,90 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
-export default function Home(){
-    const [Product_Groups, SetProduct_Groups] = useState([]);
 
-    const fetchProduct_Groups = () => {
-        fetch('/api/OutdoorFusion/all')
-          .then((response) => response.json())
-          .then((data) => {
-            SetProduct_Groups(data);
-            console.log(data);
-          })
-          .catch((error) => {
-            console.error('Error fetching product groups:', error);
-          });
-      };
-      
-      useEffect(() => {
-        fetchProduct_Groups();
-      }, []);
-    
-    return(<>
-    <h2>Different productgroups</h2>
-    <div className="Product_Groups_Container">
-        {Product_Groups ? Product_Groups.map((Product_Group) => (
-        <div className="Product_Group_Container">
-        <div>
-            <img src=""></img>
-        </div>
-        <div>
-            <h3>{Product_Group.table_name}</h3>
-            <p>Temp_Product group description</p>
-        </div>
-        </div>
-    )): null}
-    
-    </div>
-    </>)
+export default function Home() {
+  const [Product_Groups, setProductGroups] = useState([]);
+  //     const [columns, setColumns] = useState([]);
+
+
+
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch('http://localhost:5000/api/OutdoorFusion/all');
+  //       const data = await response.json();
+  //       const tableColumns = Object.entries(data).map(([table, columns]) => ({
+  //         table,
+  //         columns: Object.keys(columns[0])
+  //       }));
+  //       setColumns(tableColumns);
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error);
+  //     }
+  //   };
+
+  //   useEffect(() => {
+  //     fetchData();
+  //   }, []);
+
+  //   return (<>
+  //     <h1>Hello</h1>
+  //     <div>
+  //       {columns.map(({ table, columns }) => (
+  //         <div key={table}>
+  //           <h2>{table}</h2>
+  //           <ul>
+  //             {columns.map(column => (
+  //               <li key={column}>{column}</li>
+  //             ))}
+  //           </ul>
+  //         </div>
+  //       ))}
+  //     </div>
+  //     </>
+  //   );
+  // }
+
+
+  const fetchProductGroups = () => {
+    fetch('http://localhost:5000/api/OutdoorFusion/all')
+      .then((response) => response.json())
+      .then((data) => {
+        // Assuming data is in the same format as the previous example
+        const tableColumns = Object.entries(data).map(([table, columns]) => ({
+          table,
+          columns: Object.keys(columns[0])
+        }));
+        setProductGroups(tableColumns);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching product groups:', error);
+      });
+  };
+  
+  useEffect(() => {
+    fetchProductGroups();
+  }, []);
+  
+  return (
+    <>
+      <h2>Different product groups</h2>
+      <div className="Product_Groups_Container">
+        {Product_Groups ? (
+          Product_Groups.map((productGroup) => (
+            <div className="Product_Group_Container" key={productGroup.table}>
+              <div>
+                <h3>{productGroup.table}</h3>
+                <ul id="ColumnList">
+              {productGroup.columns ? (productGroup.columns.map((productGroup_column) => (
+                <li key={productGroup_column}>{productGroup_column}</li>
+              ))
+            ) : null}
+                </ul>
+              </div>
+            </div>
+          ))
+        ) : null}
+      </div>
+    </>
+  );
 }
