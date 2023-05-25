17c222a7d22688e1b4d89cb85e780df9f60bd9ab
JonathonStamper <96175364+JonathonStamper@users.noreply.github.com>
2023-05-25 13:43:26 +0200

    idk

--- my-app/src/Components/HomePage.js
+++ my-app/src/Components/HomePage.js
@@ -1,52 +1,54 @@
 import { useState, useEffect } from "react";
-import './Home.css';
-import {IoMdShirt} from 'react-icons/io';
+import "./Home.css";
+import { IoMdShirt } from "react-icons/io";
 
 export default function Home() {
   const [Product_Groups, setProductGroups] = useState([]);
 
   const fetchProductGroups = () => {
-    fetch('http://localhost:5000/api/OutdoorFusion/all')
+    fetch("http://localhost:5000/api/OutdoorFusion/all")
       .then((response) => response.json())
       .then((data) => {
         // Assuming data is in the same format as the previous example
         const tableColumns = Object.entries(data).map(([table, columns]) => ({
           table,
-          columns: Object.keys(columns[0])
-          
+          columns: Object.keys(columns[0]),
         }));
         setProductGroups(tableColumns);
         console.log(data);
       })
       .catch((error) => {
-        console.error('Error fetching product groups:', error);
+        console.error("Error fetching product groups:", error);
       });
   };
-  
+
   useEffect(() => {
     fetchProductGroups();
   }, []);
-  
+
   return (
     <>
       <h2>Different product groups</h2>
       <div className="Product_Groups_Container">
-        {Product_Groups ? (
-          Product_Groups.map((productGroup) => (
-            <div className="Product_Group_Container" key={productGroup.table}>
-              <div id="Product_Group_Container">
-                <h3 >{productGroup.table}</h3>
-                <ul id="ColumnList">
-              {productGroup.columns ? (productGroup.columns.map((productGroup_column) => (
-                <li key={productGroup_column}>{productGroup_column}</li>
-              ))
-            ) : null}
-                </ul>
-                <a href={`/Datachart/${productGroup.table}`}>Analyze me</a>
+        {Product_Groups
+          ? Product_Groups.map((productGroup) => (
+              <div className="Product_Group_Container" key={productGroup.table}>
+                <div id="Product_Group_Container">
+                  <h3>{productGroup.table}</h3>
+                  <ul id="ColumnList">
+                    {productGroup.columns
+                      ? productGroup.columns.map((productGroup_column) => (
+                          <li key={productGroup_column}>
+                            {productGroup_column}
+                          </li>
+                        ))
+                      : null}
+                  </ul>
+                  <a href={`/Datachart/${productGroup.table}`}>Analyze me</a>
+                </div>
               </div>
-            </div>
-          ))
-        ) : null}
+            ))
+          : null}
       </div>
 
       {/* <div><IoMdShirt/></div> */}
